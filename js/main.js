0bc362b0415c6264dab89456b8829ac00c92c337
Роман <gerasimov.roman.v@mail.ru>
2020-02-14 20:08:24 +0300

    feat: section portfolio

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+  
+});
